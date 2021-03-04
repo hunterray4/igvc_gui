0bd3a47c424dbdfe6b88ebb420ad76115bf625b2
brianprobert4 <70397449+brianprobert4@users.noreply.github.com>
2021-03-04 10:40:32 -0500

    Update userInterface.py

--- userInterface.py
+++ userInterface.py
@@ -4,41 +4,30 @@ from std_msgs.msg import Int8, UInt16MultiArray, Float64, Bool
 # from pacmod_msgs.msg import PacmodCmd, PositionWithSpeed, SystemRptInt
 # from sensor_msgs.msg import Joy
 
-drive_pub = rospy.Publisher('/selfdrive/status', Int8, queue_size=10)
-run_pub = rospy.Publisher('/gem/operation_mode', Int8, queue_size=10)
+driveParameters = rospy.Publisher('/driveParameters', Int8, queue_size=1)
 
 pygame.init()
 surface = pygame.display.set_mode((1000, 800))
 
 def set_driveMode(value1, driveMode):
-    drive_pub.publish(driveMode)
-    print(value1)
+    # Do the job here !
     pass
 
 
 def set_run_modes(value2, run_modes):
-    run_pub.publish(run_modes)
-    print(value2)
     # Do the job here !
         pass
 
 
 def start_the_game():
-    drive_pub.publish(driveMode)
-    run_pub.publish(run_modes)
+    driveParameters.publish(value1, value2)
     pass
 
 menu = pygame_menu.Menu(800, 1000, 'AVRAD User Interface', theme=pygame_menu.themes.THEME_GREEN)
 
-<<<<<<< HEAD
 menu.add_selector('Drive Mode :', [('Manual Drive', 0), ('Tele-Op', 1), ('Autonomous Self-Drive', 2)], onchange=set_driveMode)
-menu.add_selector('Topic :', [('NOT AUTONOMOUS MODE', 6), ('Regular Self-drive', 0), ('E-Stop Qualification', 1), ('Straight Lane Keeping Qualification', 2), 
-        ('Left Turn Qualification', 3), ('Right Turn Qualification', 4), ('Stop Sign Detection Qualification', 5)], onchange=set_topic)
-=======
-menu.add_selector('Drive Mode :', [('Manuel Drive', 0), ('Tele-Op', 1), ('Autonomous Self-Drive', 2)], onchange=set_driveMode)
 menu.add_selector('Run Mode :', [('NOT AUTONOMOUS MODE', 6), ('Regular Self-drive', 0), ('E-Stop Qualification', 1), ('Straight Lane Keeping Qualification', 2), 
         ('Left Turn Qualification', 3), ('Right Turn Qualification', 4), ('Stop Sign Detection Qualification', 5)], onchange=set_run_modes)
->>>>>>> e422431d87a39e1e8de8e29ca5cfe5128fc55f16
 menu.add_button('Run', start_the_game)
 menu.add_button('Quit', pygame_menu.events.EXIT)
 menu.mainloop(surface)
